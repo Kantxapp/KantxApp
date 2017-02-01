bf2d4db57fe1661984e3a0d44326e68e47763ec2
hblg <hafsatanger@hotmail.fr>
2017-02-01 11:07:24 +0000

    01/02/2017-12:04-Hafsa

--- resources/views/layouts/mapa.blade.php
+++ resources/views/layouts/mapa.blade.php
@@ -27,133 +27,132 @@
   <link rel="stylesheet" href="/assets/mobirise/css/mbr-additional.css" type="text/css">
   <link rel="stylesheet" href="/assets/mobirise2/css/mbr-additional.css" type="text/css">
   <link rel="stylesheet" href="/assets/theme2/css/style.css">
+
   <script src="https://code.jquery.com/jquery-1.11.1.js" ></script>
-
-<script type="text/javascript">
-  var XmlHttpObj;
-  var Utf8 = {
-    //Convierte de UTF-8 a ISO
-    decode : function (utftext) {
-        var string = "";
-        var i = 0;
-        var c = c1 = c2 = 0;
-
-        while ( i < utftext.length ) {
-
-            c = utftext.charCodeAt(i);
-
-            if (c < 128) {
-                string += String.fromCharCode(c);
-                i++;
-            }
-            else if((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i+1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            }
-            else {
-                c2 = utftext.charCodeAt(i+1);
-                c3 = utftext.charCodeAt(i+2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-
-        }
-
-        return string;
+<style>
+       #map {
+        height: 600px;
+        width: 759px;
+       }
+       .controls {
+      margin-top: 10px;
+      border: 1px solid transparent;
+      border-radius: 2px 0 0 2px;
+      box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      height: 32px;
+      outline: none;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+    }
+    
+    #pac-input {
+      background-color: #fff;
+      font-family: Roboto;
+      font-size: 15px;
+      font-weight: 300;
+      margin-left: 12px;
+      padding: 0 11px 0 13px;
+      text-overflow: ellipsis;
+      width: 300px;
+    }
+    
+    #pac-input:focus {
+      border-color: #4d90fe;
+    }
+    
+    .pac-container {
+      font-family: Roboto;
+    }
+    
+    #type-selector {
+      color: #fff;
+      background-color: #4d90fe;
+      padding: 5px 11px 0px 11px;
+    }
+    
+    #type-selector label {
+      font-family: Roboto;
+      font-size: 13px;
+      font-weight: 300;
     }
 
+    </style>
+<script type="text/javascript">
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.3275473, lng: -1.9712059},
+    zoom: 13
+  });
+  var input = /** @type {!HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      autocomplete.setTypes(types);
+    });
+  }
+
+  setupClickListener('changetype-all', []);
+  setupClickListener('changetype-address', ['address']);
+  setupClickListener('changetype-establishment', ['establishment']);
+  setupClickListener('changetype-geocode', ['geocode']);
 }
-
-function CreateXmlHttpObj()
-{
-	try
-	{
-		XmlHttpObj = new ActiveXObject("Msxml2.XMLHTTP");
-	
-	}
-	catch(e)
-	{
-		try
-		{
-			XmlHttpObj = new ActiveXObject("Microsoft.XMLHTTP");
-		} 
-		catch(oc)
-		{
-			XmlHttpObj = null;
-		}
-	}
-		if(!XmlHttpObj && typeof XMLHttpRequest != "undefined") 
-	{
-		XmlHttpObj = new XMLHttpRequest();
-	}
-}
-
-function provinciaListOnChange() {
-
-    var provinciaList = document.getElementById("provinciaList");
- 
-    var selectedprovincia = provinciaList.options[provinciaList.selectedIndex].value;
-    var requestUrl;
-
-    //requestUrl = "data.php" + "?filter=" + encodeURIComponent(selectedprovincia);
-    requestUrl = "/localidades/" + selectedprovincia;
-    
-	CreateXmlHttpObj();
-	
-	if(XmlHttpObj)
-	{
-	
-		XmlHttpObj.onreadystatechange = StateChangeHandler;
-		XmlHttpObj.open( "GET", requestUrl, true );
-		XmlHttpObj.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-		XmlHttpObj.send("");		
-	}
-}
-
-
-function StateChangeHandler()
-{
-	if(XmlHttpObj.readyState == 4)
-	{
-		if(XmlHttpObj.status == 200)
-		{			
-			PopulatelocalidadList(XmlHttpObj.responseXML.documentElement);
-		}
-		else
-		{
-			alert("Código de error: "  + XmlHttpObj.status);
-		}
-	}
-}
-
-function PopulatelocalidadList(localidadNode)
-{	
-    var localidadList = document.getElementById("localidadList");
-	for (var count = localidadList.options.length-1; count >-1; count--)
-	{
-		localidadList.options[count] = null;
-	}
-
-	var localidadNodes = localidadNode.getElementsByTagName('localidad');
-	var textValue; 
-	var optionItem;
-	for (var count = 0; count < localidadNodes.length; count++)
-	{ 
-   		textValue = Utf8.decode(GetInnerText(localidadNodes[count]));
-		idValue=count;		
-		optionItem = new Option( textValue, textValue,  false, false);
-		localidadList.options[localidadList.length] = optionItem;
-	}
-var to=document.getElementById("advice");
-to.innerHTML="";
-}
-
-function GetInnerText (node)
-{
-	 return (node.textContent || node.innerText || node.text) ;
-}
-
 
 </script>
   @yield('head')
