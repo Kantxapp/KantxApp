cd5c59346d911ffe2cefc71a450d8b510a46aba3
hblg <hafsatanger@hotmail.fr>
2017-02-03 09:05:12 +0000

    03/02/2017-10:02-Hafsa

--- resources/views/deportes.blade.php
+++ resources/views/deportes.blade.php
@@ -14,28 +14,28 @@
         <div class="col-sm-6 col-md-12" id="imgDeportes">
                 <h1 class="neweventh1">Selecciona tus deportes</h1>
                     <p>Selecciona los deportes que quieres añadir a tu perfil:</p>
-                      <img id="myImg" src="/assets/images/Deportes/futbol7.png" class="img-thumbnail" alt="Responsive image" data-toggle="modal" data-target="#exampleModal"/>
-                      <img src="/assets/images/Deportes/balonmano.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/bicicleta-carretera.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/bicicleta-montana.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/frontenis.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/futbol7.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/futbol11.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/futbol_sala_asfalto.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/judo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/karate.png" class="img-thumbnail"  alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/motociclismo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/padel.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/pilates.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/running.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/senderismo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
+                      <img src="/assets/images/Deportes/futbol7.png" class="img-thumbnail" alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Futbol con siete jugadores"/>
+                      <img src="/assets/images/Deportes/balonmano.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Balonmano">
+                      <img src="/assets/images/Deportes/bicicleta-carretera.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Bicicleta de carretera">
+                      <img src="/assets/images/Deportes/bicicleta-montana.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="bicicleta de montaña">
+                      <img src="/assets/images/Deportes/frontenis.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Frontenis">
+                      <img src="/assets/images/Deportes/futbol11.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Futbol con once jugadores">
+                      <img src="/assets/images/Deportes/futbol_sala_asfalto.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Futbol sala">
+                      <img src="/assets/images/Deportes/judo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Judo">
+                      <img src="/assets/images/Deportes/karate.png" class="img-thumbnail"  alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Karate">
+                      <img src="/assets/images/Deportes/motociclismo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Motociclismo">
+                      <img src="/assets/images/Deportes/padel.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Padel">
+                      <img src="/assets/images/Deportes/natacion.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Natacion">
+                      <img src="/assets/images/Deportes/pilates.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Pilates">
+                      <img src="/assets/images/Deportes/running.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Running">
+                      <img src="/assets/images/Deportes/senderismo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Senderismo">
                       </br>
                       </br>
-                      <img src="/assets/images/Deportes/spinning.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/squash.png" class="img-thumbnail"  alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/taekwondo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/tenis.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
-                      <img src="/assets/images/Deportes/voleibol.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal">
+                      <img src="/assets/images/Deportes/spinning.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Spinning">
+                      <img src="/assets/images/Deportes/squash.png" class="img-thumbnail"  alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Squash">
+                      <img src="/assets/images/Deportes/taekwondo.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Taekwondo">
+                      <img src="/assets/images/Deportes/tenis.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Tenis">
+                      <img src="/assets/images/Deportes/voleibol.png" class="img-thumbnail"   alt="Responsive image" data-toggle="modal" data-target="#exampleModal" data-toggle="tooltip" data-placement="left" title="Voleibol">
           
                     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
                       <div class="modal-dialog" role="document">
