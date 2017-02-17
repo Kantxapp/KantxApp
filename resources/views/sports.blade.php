30b4b92779dba473f6523ced483fd9e6e8dace92
Iker <iker_izagirre@hotmail.com>
2017-02-17 09:53:26 +0000

    17/02/2017-10:49-Iker_Izaguirre-Sports_Blade

--- resources/views/sports.blade.php
+++ resources/views/sports.blade.php
@@ -3,18 +3,27 @@
 
 @section('content')
 
-<div class="container" style="margin-top: 90px;">
-  <h1 class="text-center">¡Bienvenido a KantxApp!</h1><br/>
-  <h3>Por favor, selecciona los deportes que te interesan a continuación</h3>
-  <div class="row">
-    @foreach($sports as $sport)
-      <div id="userId={{$user_id}}_sportId={{$sport->id}}" class="col-md-3 col-xs-3 col-lg-3 sport">
-      <img src='{{$sport->img_path}}'><br>
-      {{$sport->name}}
-      </div>
-    @endforeach
+
+    <div class="container" style="margin-top: 90px;">
+      
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-xs-center">
+              <!--<div class="container" style="margin-top: 90px;">-->
+                <h1 class="text-center">¡Bienvenido a KantxApp!</h1><br/>
+                <h3>Por favor, selecciona los deportes que te interesan a continuación</h3>
+                <div class="row">
+                  @foreach($sports as $sport)
+                    <div id="userId={{$user_id}}_sportId={{$sport->id}}" class="col-md-3 col-xs-3 col-lg-3 sport">
+                    <img src='{{$sport->img_path}}'><br>
+                    {{$sport->name}}
+                    </div>
+                  @endforeach
+                  </div>
+              <!--</div>-->
+                <div style="margin-top: 10px;"><a class="btn btn-secondary greenery" href="{{ url('/home') }}">CONTINUAR</a></div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('script')
